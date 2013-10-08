f0c678e3fda9ddbc4c5f431d8c48c1bbfe7018f4
Kurtis Kemple <kurtiskemple@gmail.com>
2013-10-08 11:47:01 -0400

    updated namespace and docs

--- docs/api.js
+++ docs/api.js
@@ -1,23 +1,23 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "PIX.app",
-        "PIX.app.return",
-        "PIX.core",
-        "PIX.core.dom",
-        "PIX.core.dom.form",
-        "PIX.core.return",
-        "PIX.core.util",
-        "PIX.sandbox",
-        "PIX.sandbox.return"
+        "MOD.app",
+        "MOD.app.return",
+        "MOD.core",
+        "MOD.core.dom",
+        "MOD.core.dom.form",
+        "MOD.core.return",
+        "MOD.core.util",
+        "MOD.sandbox",
+        "MOD.sandbox.return"
     ],
     "modules": [
-        "PIX"
+        "MOD"
     ],
     "allModules": [
         {
-            "displayName": "PIX",
-            "name": "PIX",
+            "displayName": "MOD",
+            "name": "MOD",
             "description": "The application namespace."
         }
     ]
